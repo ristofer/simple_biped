fa58935b9c6a05d0d6a32acd2498058fd1084c77
Your Name <you@example.com>
2017-12-07 10:23:51 -0300

    asfdasf

--- src/simple_biped/leg.py
+++ src/simple_biped/leg.py
@@ -16,10 +16,8 @@ import math
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectoryPoint
 from control_msgs.msg import (FollowJointTrajectoryAction, FollowJointTrajectoryGoal)
-# Robot skill
-from uchile_skills.robot_skill import RobotSkill
 
-class LegSkill(RobotSkill):
+class LegSkill(object):
     """
     Base class for joint space control using joint trajectory action.
     """
@@ -51,7 +49,6 @@ class LegSkill(RobotSkill):
             TypeError: If `arm_name` is not a string.
             ValueError: If `arm_name` is not "L_LEG" or "R_LEG".
         """
-        super(LegSkill, self).__init__()
         self._description = "Control using joint trajectory action"
         # Check arm name
         if not isinstance(arm_name, str):
@@ -119,22 +116,22 @@ class LegSkill(RobotSkill):
         jta_client = actionlib.SimpleActionClient(self._jta_topic, FollowJointTrajectoryAction)
         # Wait for the JTA server to start or exit
         if not jta_client.wait_for_server(timeout=rospy.Duration(timeout)):
-            self.logerr("Joint trajectory action server for \"{0}\" not found".format(self.name))
+            rospy.logerr("Joint trajectory action server for \"{0}\" not found".format(self.name))
             return False
-        self.log.debug("Joint trajectory action server for \"{0}\" [OK]".format(self.name))
+        rospy.logdebug("Joint trajectory action server for \"{0}\" [OK]".format(self.name))
         # Check joint_states topic
         try:
             msg = rospy.wait_for_message(self._joint_state_topic, JointState, timeout=timeout)
         except rospy.ROSException:
-            self.logerr("Topic \"{0}\" not already published".format(self._joint_state_topic))
+            rospy.logerr("Topic \"{0}\" not already published".format(self._joint_state_topic))
             return False
         # Check arm joints in message
         for joint in self.joint_names:
             if not joint in msg.name:
-                self.logerr("Topic \"{0}\" does not contain \"{1}\" joints".format(
+                rospy.logerr("Topic \"{0}\" does not contain \"{1}\" joints".format(
                     self._joint_state_topic, self.name))
                 return False
-        self.logdebug("Topic \"{0}\" published [OK]".format(self._joint_state_topic))
+        rospy.logdebug("Topic \"{0}\" published [OK]".format(self._joint_state_topic))
      
         return True
 
@@ -149,7 +146,7 @@ class LegSkill(RobotSkill):
         return True
 
     def shutdown(self):
-        self.logwarn("Shutdown \"{0}\" skill, calling cancel goals...".format(self.name))
+        rospy.logwarn("Shutdown \"{0}\" skill, calling cancel goals...".format(self.name))
         # Cancel goals
         self._jta_client.cancel_all_goals()
         # Unregister subscriber
@@ -157,11 +154,11 @@ class LegSkill(RobotSkill):
         return True
 
     def start(self):
-        self.logdebug("Start \"{0}\" skill".format(self.name))
+        rospy.logdebug("Start \"{0}\" skill".format(self.name))
         return True
 
     def pause(self):
-        self.logdebug("Pause \"{0}\" skill".format(self.name))
+        rospy.logdebug("Pause \"{0}\" skill".format(self.name))
         self.stop()
         return True
 
@@ -170,7 +167,7 @@ class LegSkill(RobotSkill):
         """
         Cancel all goals.
         """
-        self.logwarn("Stop \"{0}\", calling cancel goals...".format(self.name))
+        rospy.logwarn("Stop \"{0}\", calling cancel goals...".format(self.name))
         self._jta_client.cancel_all_goals()
 
     def send_joint_goal(self, joint_goal, interval=3.0, segments=10):
@@ -191,7 +188,7 @@ class LegSkill(RobotSkill):
         rospy.sleep(0.05)
         current_state = self.get_joint_state()
         if (rospy.Time.now() - current_state.header.stamp) > rospy.Duration(1.0):
-            self.logerr("Current position has not been updated, check \"{}\" topic.".format(self._joint_state_topic))
+            rospy.logerr("Current position has not been updated, check \"{}\" topic.".format(self._joint_state_topic))
             return
         # Create new goal
         goal = FollowJointTrajectoryGoal()
@@ -209,7 +206,7 @@ class LegSkill(RobotSkill):
             point.time_from_start = rospy.Duration(t)
             goal.trajectory.points.append(point)
         # Send goal to JTA
-        self.loginfo('Sending new goal for \"{0}\"'.format(self.name))
+        rospy.loginfo('Sending new goal for \"{0}\"'.format(self.name))
         self._jta_client.send_goal(goal)
 
     def wait_for_motion_done(self, timeout=0.0):
@@ -222,7 +219,7 @@ class LegSkill(RobotSkill):
         Returns:
             bool: True if the goal finished. False if the goal didn't finish within the allocated timeout.
         """
-        self.log.info('Waiting for \"{0}\" motion'.format(self.name))
+        rospy.log.info('Waiting for \"{0}\" motion'.format(self.name))
         return self._jta_client.wait_for_result(rospy.Duration(timeout))
 
     def get_result(self):
